4d2f3e76e976a0a239a8240b8cb3ffaa42386911
ritusinha128 <50213321+ritusinha128@users.noreply.github.com>
2019-08-01 01:26:23 +0530

    Add files via upload

---
+++ self_om.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 12:08:25 2019
+
+@author: Pooja
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Credit_Card_Applications.csv')
+
+X = dataset.iloc[:,:-1].values
+Y = dataset.iloc[:,-1].values
+
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range=(0, 1))
+X = sc.fit_transform(X)
+
+from minisom import MiniSom
+som = MiniSom(x = 10, y =10, input_len = 15, sigma = 1.0, learning_rate=0.5)
+som.random_weights_init(X)
+som.train_random(X, 100)
+
+#Visualization
+from pylab import bone, pcolor, colorbar, plot, show
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o','s']
+colors = ['red', 'green']
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0]+0.5, w[1]+0.5, markers[Y[i]], markeredgecolor = colors[Y[i]], markerfacecolor = 'None', markersize = 10, markeredgewidth = 2 )
+show()
+
+#Finding frauds
+mappings = som.win_map(X)
+frauds = np.concatenate ((mappings[(8,1)], mappings[(6,8)]), axis=0)
+frauds = sc.inverse_transform(frauds)
